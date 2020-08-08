2fd01ea68434ecac3e9095debfe6f82d3bf264f0
Ryan_Wang <zw2315@nyu.edu>
2020-08-08 15:59:11 -0400

    add

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -6,7 +6,9 @@ import com.how2java.tmall.util.Page;
 import java.util.List;
 
 public interface CategoryMapper {
-    public List<Category> list(Page page);
-    public int total();
+    List<Category> list(Page page);
+    int total();
+    void add(Category category);
+
 
 }
