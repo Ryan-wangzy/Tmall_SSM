bcd9c2c9ba1dcb17284b60ee5f1aade4763d27be
Ryan_Wang <zw2315@nyu.edu>
2020-08-22 14:51:46 -0400

    CartPage & LoginInterceptor

---
+++ src/main/java/com/how2java/tmall/interceptor/LoginInterceptor.java
@@ -0,0 +1,61 @@
+package com.how2java.tmall.interceptor;
+
+import com.how2java.tmall.pojo.User;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+
+    /**
+     * 在业务处理器处理请求之前被调用
+     * 如果返回false
+     *     从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
+     * 如果返回true
+     *    执行下一个拦截器,直到所有的拦截器都执行完毕
+     *    再执行被拦截的Controller
+     *    然后进入拦截器链,
+     *    从最后一个拦截器往回执行所有的postHandle()
+     *    接着再从最后一个拦截器往回执行所有的afterCompletion()
+     */
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        String contextPath = session.getServletContext().getContextPath();
+        String[] noNeedAuthPage = new String[]{
+                "home",
+                "checkLogin",
+                "register",
+                "loginAjax",
+                "login",
+                "product",
+                "category",
+                "search"};
+
+        String uri = request.getRequestURI();
+        uri = StringUtils.remove(uri,contextPath);
+        if(uri.startsWith("/fore")){
+            //获取uri字符串内，分隔符“/fore”后的字符串
+            String method = StringUtils.substringAfterLast(uri,"/fore");
+            if(!Arrays.asList(noNeedAuthPage).contains(method)){
+                User user = (User) session.getAttribute("user");
+                if(null == user){
+                    response.sendRedirect("loginPage");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
